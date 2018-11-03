0d107e051f20fe280c80b34a35dc72a1754dabd7
Kazuki Iwata <kazu.0516.k0n0f@gmail.com>
2018-11-03 23:38:21 +0900

    :+1: uniqueIdをprintに追加

--- honeypot.py
+++ honeypot.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import hashlib
+
 from scapy.all import *
 from scapy.layers.inet import IP, TCP, Raw
 
@@ -21,11 +23,16 @@ def find_tcp_packet(packet: Packet):
     tcp = packet[TCP]
     flags = tcp.flags
 
-    print(f"[Recv sniffed packet: {flags}] {ip.src}:{tcp.sport} -> {ip.dst}:{tcp.dport}")
-
-    # データもってたら表示してみる
-    if packet.haslayer(Raw):
-        packet[Raw].show()
+    if ip.src == TARGET_IP_ADDRESS:
+        uid = hashlib.sha256(f"{ip.src}:{tcp.sport} > {ip.dst}:{tcp.dport}".encode()).hexdigest()
+        print(f"[Packet] {uid} Send {packet.summary()}")
+    else:
+        uid = hashlib.sha256(f"{ip.dst}:{tcp.dport} > {ip.src}:{tcp.sport}".encode()).hexdigest()
+        # データもってたら表示してみる
+        if packet.haslayer(Raw):
+            print(f"[Packet] {uid} Recv {packet.summary()} {packet[Raw].load}")
+        else:
+            print(f"[Packet] {uid} Recv {packet.summary()}")
 
     if flags == "S":
         i = IP(src=ip.dst, dst=ip.src)
@@ -33,15 +40,11 @@ def find_tcp_packet(packet: Packet):
         frame = i / t
         send(frame, verbose=False)
 
-        print(f"[Send dummy packet: SYN/ACK] {i.src}:{t.sport} -> {i.dst}:{t.dport}")
-
     elif 'P' in list(str(flags)):
         i = IP(src=ip.dst, dst=ip.src)
         t = TCP(sport=tcp.dport, dport=tcp.sport, flags='A', seq=tcp.ack, ack=(tcp.seq + len(packet)))
         frame = i / t
         send(frame, verbose=False)
-
-        print(f"[Send dummy packet: ACK] {i.src}:{t.sport} -> {i.dst}:{t.dport}")
 
 
 if __name__ == "__main__":
